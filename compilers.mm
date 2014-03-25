2b58a46794aa5b4fea1cf31bf1226bb311b9b676
Andrew Kanner <andrew.kanner@gmail.com>
2014-03-25 13:03:21 +0400

    Initial commit. Added mindmap for first 3 chapters of cs143 course

---
+++ compilers.mm
@@ -0,0 +1,192 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry RESTRICTED="true"/>
+<node CREATED="1395732899000" ID="ID_753711553" MODIFIED="1395735580476" TEXT="Compiler">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1395732913379" ID="ID_715608857" MODIFIED="1395735729876" POSITION="right" TEXT="1. Lexical Analysis">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1395733470130" FOLDED="true" ID="ID_1442877164" MODIFIED="1395735687673" TEXT="&#x446;&#x435;&#x43b;&#x44c; - &#x442;&#x43e;&#x43a;&#x435;&#x43d;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f; &#x438;&#x441;&#x445;&#x43e;&#x434;&#x43d;&#x43e;&#x433;&#x43e; &#x442;&#x435;&#x43a;&#x441;&#x442;&#x430; &#x43f;&#x43e; &#x43a;&#x43b;&#x430;&#x441;&#x441;&#x430;&#x43c;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1395733295001" FOLDED="true" ID="ID_426853348" MODIFIED="1395735679583" TEXT="Identifier">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734712382" ID="ID_123771322" MODIFIED="1395734738746" TEXT="example - variables">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1395734731420" ID="ID_763308298" MODIFIED="1395734736146" TEXT="start with the letter"/>
+</node>
+<node CREATED="1395733302068" FOLDED="true" ID="ID_436534699" MODIFIED="1395735680986" TEXT="Integer">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734753054" ID="ID_1481430313" MODIFIED="1395734761757" TEXT="example - numbers">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1395733307038" FOLDED="true" ID="ID_750631142" MODIFIED="1395735682330" TEXT="Keywords">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734778982" ID="ID_716139081" MODIFIED="1395734785017" TEXT="if"/>
+<node CREATED="1395734788140" ID="ID_1520449978" MODIFIED="1395734789881" TEXT="else"/>
+<node CREATED="1395734790492" ID="ID_945292627" MODIFIED="1395734792185" TEXT="..."/>
+</node>
+<node CREATED="1395733314869" FOLDED="true" ID="ID_637901189" MODIFIED="1395735683828" TEXT="Whitespaces">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734818349" ID="ID_1408981803" MODIFIED="1395734849811" TEXT="&apos; &apos;"/>
+<node CREATED="1395734823940" ID="ID_1777958541" MODIFIED="1395734849458" TEXT="&apos;   &apos;"/>
+<node CREATED="1395734829516" ID="ID_250397758" MODIFIED="1395734849068" TEXT="&apos;\t\t&apos;"/>
+<node CREATED="1395734834388" ID="ID_1878805343" MODIFIED="1395734835849" TEXT="..."/>
+</node>
+<node CREATED="1395733318782" FOLDED="true" ID="ID_427720600" MODIFIED="1395735685092" TEXT="Operators">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734865054" ID="ID_1290785159" MODIFIED="1395734870209" TEXT="&apos;==&apos;"/>
+<node CREATED="1395734870644" ID="ID_1843072718" MODIFIED="1395734873513" TEXT="..."/>
+</node>
+<node CREATED="1395733571880" FOLDED="true" ID="ID_1843405624" MODIFIED="1395735686487" TEXT="Others">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734896472" ID="ID_1290712340" MODIFIED="1395734910337" TEXT="&apos;(&apos;"/>
+<node CREATED="1395734910788" ID="ID_975290839" MODIFIED="1395734913970" TEXT="&apos;)&apos;"/>
+<node CREATED="1395734914820" ID="ID_422853177" MODIFIED="1395734918353" TEXT="&apos;;&apos;"/>
+<node CREATED="1395734918652" ID="ID_1627703080" MODIFIED="1395734963249" TEXT="&apos;=&apos;, assignment"/>
+<node CREATED="1395734940060" ID="ID_225278592" MODIFIED="1395734945233" TEXT="&apos;++&apos;"/>
+<node CREATED="1395734945724" ID="ID_1592018082" MODIFIED="1395734949703" TEXT="&apos;&lt;&apos;"/>
+<node CREATED="1395734949932" ID="ID_210328659" MODIFIED="1395734950849" TEXT="..."/>
+</node>
+</node>
+<node CREATED="1395733552238" ID="ID_208208382" MODIFIED="1395734261466" TEXT="token ~ &lt;class, substring&gt;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1395733767948" FOLDED="true" ID="ID_770917127" MODIFIED="1395736976785" TEXT="substring ~ lexem">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395734985927" ID="ID_1527187316" MODIFIED="1395735408701" TEXT="&#x43f;&#x440;&#x435;&#x43e;&#x431;&#x440;&#x430;&#x437;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x44f; &#x442;&#x438;&#x43f;&#x43e;&#x432; &#x435;&#x449;&#x435; &#x43d;&#x435;&#x442;, &#x438;&#x441;&#x43f;&#x43e;&#x43b;&#x44c;&#x437;&#x443;&#x435;&#x442;&#x441;&#x44f; &#x438;&#x43c;&#x435;&#x43d;&#x43d;&#x43e; &#x43f;&#x43e;&#x434;&#x441;&#x442;&#x440;&#x43e;&#x43a;&#x430;">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1395733838580" FOLDED="true" ID="ID_515065119" MODIFIED="1395737468727" TEXT="&#x432; &#x445;&#x43e;&#x434;&#x435; &#x430;&#x43d;&#x430;&#x43b;&#x438;&#x437;&#x430; &#x441;&#x43b;&#x435;&#x432;&#x430; &#x43d;&#x430; &#x43f;&#x440;&#x430;&#x432;&#x43e; &#x43c;&#x43e;&#x436;&#x435;&#x442; &#x43f;&#x43e;&#x442;&#x440;&#x435;&#x431;&#x43e;&#x432;&#x430;&#x442;&#x44c;&#x441;&#x44f; lookahead">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1395734483277" FOLDED="true" ID="ID_1115622809" MODIFIED="1395736972734" TEXT="Fortran">
+<icon BUILTIN="attach"/>
+<node CREATED="1395735069951" ID="ID_823643936" MODIFIED="1395735201534" TEXT="whitespace is insignificant (VAR1 ~ VAR  R1)">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1395735080316" ID="ID_1103286838" MODIFIED="1395735082273" TEXT="&quot;DO 5 I = 1.25&quot; &#x438; &quot;DO 5 I = 1,25&quot; (lookahead &#x43d;&#x443;&#x436;&#x435;&#x43d; &#x43f;&#x43e;&#x441;&#x43b;&#x435; &#x43f;&#x440;&#x43e;&#x447;&#x442;&#x435;&#x43d;&#x438;&#x44f; DO)"/>
+</node>
+<node CREATED="1395735159428" FOLDED="true" ID="ID_1217618207" MODIFIED="1395736973869" TEXT="PL/I">
+<icon BUILTIN="attach"/>
+<node CREATED="1395735171787" ID="ID_609323446" MODIFIED="1395735205367" TEXT="keywords are not reserved">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1395735208692" ID="ID_1585371513" MODIFIED="1395735230689" TEXT="IF ELSE THEN THEN=ELSE; ELSE ELSE=THEN"/>
+</node>
+<node CREATED="1395735243340" FOLDED="true" ID="ID_719465345" MODIFIED="1395736975224" TEXT="C++">
+<icon BUILTIN="attach"/>
+<node CREATED="1395735255970" ID="ID_1097987727" MODIFIED="1395735379575" TEXT="Foo &lt; Bar &lt; Bazz &gt;&gt; (is this output redirection?)"/>
+<node CREATED="1395735380852" ID="ID_580105646" MODIFIED="1395735410870" TEXT=" For classes there should be &apos; &apos; between &apos;&gt;&apos; and &apos;&gt;&apos;">
+<icon BUILTIN="info"/>
+</node>
+</node>
+</node>
+<node CREATED="1395735729877" FOLDED="true" ID="ID_667121816" MODIFIED="1395737690859" TEXT="Regular languages">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395735976733" ID="ID_1578933450" MODIFIED="1395735990898" TEXT="&#x3b5; - language empty string"/>
+<node CREATED="1395736443636" ID="ID_1672058834" MODIFIED="1395736466457" TEXT="&#x2211;* - all symbols of alphabet"/>
+<node CREATED="1395735992964" FOLDED="true" ID="ID_704916855" MODIFIED="1395736991579" TEXT="Regular expression is the smallest set of expressions including">
+<node CREATED="1395736033056" ID="ID_871423656" MODIFIED="1395736285293" TEXT="&#x3b5; | &apos;c&apos; c&#x20ac;&#x2211; | R+R | RR | R*"/>
+<node CREATED="1395736496100" ID="ID_1205758985" MODIFIED="1395736510469" TEXT="all this calls grammar">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1395736292076" ID="ID_242422107" MODIFIED="1395736405866" TEXT="union A+B = A U B"/>
+<node CREATED="1395736314740" ID="ID_713601961" MODIFIED="1395736399319" TEXT="concatenation AB = {ab | a&#x20ac;A, b&#x20ac;B}"/>
+<node CREATED="1395736308459" ID="ID_123154210" MODIFIED="1395736425762" TEXT="iteration A* = UA^i (i &gt;=0)"/>
+</node>
+<node CREATED="1395736527620" FOLDED="true" ID="ID_92724056" MODIFIED="1395736812326" TEXT="some rules for regular expressions">
+<icon BUILTIN="info"/>
+<node CREATED="1395736560677" ID="ID_1911105913" MODIFIED="1395736572594" TEXT="(a+b)a = aa + ba"/>
+<node CREATED="1395736572908" ID="ID_386852844" MODIFIED="1395736583218" TEXT="1* = 1* + 1"/>
+<node CREATED="1395736583459" ID="ID_257219354" MODIFIED="1395736623690" TEXT="0* ~ 0 + 0* ~ &#x3b5; + 00* ~ &#x3b5; + 0 + 0*"/>
+</node>
+</node>
+<node CREATED="1395735760604" FOLDED="true" ID="ID_349259012" MODIFIED="1395738010055" TEXT="Formal languages">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395736677974" FOLDED="true" ID="ID_1950882458" MODIFIED="1395736804913" TEXT="Meaning function">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395736700638" ID="ID_788524825" MODIFIED="1395736703873" TEXT="L(e) = M"/>
+<node CREATED="1395736704108" ID="ID_1809310108" MODIFIED="1395736714026" TEXT="regexp ~ set of strings"/>
+</node>
+<node CREATED="1395736745627" ID="ID_672781964" MODIFIED="1395736755447" TEXT="L(&#x3b5;) = {&quot;&quot;}"/>
+<node CREATED="1395736756380" ID="ID_617914218" MODIFIED="1395736768986" TEXT="L(&apos;c&apos;) = {&quot;c&quot;}"/>
+<node CREATED="1395736774180" ID="ID_1115571314" MODIFIED="1395736784865" TEXT="L(A+B) = L(A) + L(B)"/>
+<node CREATED="1395736785076" ID="ID_455888937" MODIFIED="1395736833650" TEXT="L(AB) = {ab | a&#x20ac;L(A), b&#x20ac;L(B)}"/>
+<node CREATED="1395736834228" ID="ID_93971489" MODIFIED="1395736863034" TEXT="L(A*) = UL(A)^i (i&gt;=0)"/>
+<node CREATED="1395736866524" FOLDED="true" ID="ID_80457407" MODIFIED="1395737462274" TEXT="(Syntax -&gt; Semantics) is &apos;many to one&apos; relation">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+<node CREATED="1395736925033" ID="ID_1527093788" MODIFIED="1395737460885" TEXT="so there`s possible optimization"/>
+</node>
+</node>
+<node CREATED="1395735805955" FOLDED="true" ID="ID_1282552180" MODIFIED="1395738006959" TEXT="Lexical specification">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1395736998693" FOLDED="true" ID="ID_887275241" MODIFIED="1395737406920" TEXT="Integers">
+<node CREATED="1395737062242" ID="ID_135600944" MODIFIED="1395737113977" TEXT="digit = &apos;0&apos; + &apos;1&apos; + &apos;2&apos; + &apos;3&apos; + &apos;4&apos; + &apos;5&apos; + &apos;6&apos; + &apos;7&apos; + &apos;8&apos; + &apos;9&apos;"/>
+<node CREATED="1395737114732" ID="ID_1968050662" MODIFIED="1395737199496" TEXT="integers = digit digit* (should not be empty)"/>
+<node CREATED="1395737199940" ID="ID_1044525424" MODIFIED="1395737214268" TEXT="integers = digit^+">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1395737023876" FOLDED="true" ID="ID_114849561" MODIFIED="1395737376176" TEXT="Identifiers">
+<node CREATED="1395737218264" ID="ID_1678140557" MODIFIED="1395737317690" TEXT="letter = &apos;a&apos; + &apos;b&apos; + ... + &apos;z&apos; + &apos;A&apos; + &apos;B&apos; + ... + &apos;Z&apos;"/>
+<node CREATED="1395737318212" ID="ID_1663258422" MODIFIED="1395737360006" TEXT="letter = [a-zA-Z] - character range">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1395737335396" ID="ID_186280480" MODIFIED="1395737355624" TEXT="identifiers = letter(letter+digit)*">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1395737030028" FOLDED="true" ID="ID_1923846989" MODIFIED="1395737415529" TEXT="Whitespaces">
+<node CREATED="1395737380131" ID="ID_1390343794" MODIFIED="1395737414363" TEXT="whitespaces = (&apos; &apos; + &apos;\n&apos; + &apos;\t&apos;)^+">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1395737036796" FOLDED="true" ID="ID_931831765" MODIFIED="1395738005215" TEXT="Keywords">
+<node CREATED="1395737230421" ID="ID_1685558497" MODIFIED="1395737269962" TEXT="&apos;i&apos;&apos;f&apos; + &apos;e&apos;&apos;l&apos;&apos;s&apos;&apos;e&apos; + ..."/>
+<node CREATED="1395737246331" ID="ID_578531618" MODIFIED="1395737282542" TEXT="&apos;if&apos; +&apos;else&apos; + &apos;then&apos; + ...">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1395737422940" FOLDED="true" ID="ID_1270336309" MODIFIED="1395737687388" TEXT="Pascal lexical specification">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+<node CREATED="1395737439801" ID="ID_1098736850" MODIFIED="1395737494994" TEXT="num = digits opt_fraction opt_exponent">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1395737495765" ID="ID_923953770" MODIFIED="1395737571946" TEXT="opt_fraction = (&apos;.&apos;digits) + &#x3b5; - this means that it can be absent (optional)"/>
+<node CREATED="1395737549948" ID="ID_600027343" MODIFIED="1395737575683" TEXT="opt_fraction = (&apos;.&apos;digits)?">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1395737576861" ID="ID_1266267181" MODIFIED="1395737673072" TEXT="opt_exponent = (&apos;E&apos;(&apos;+&apos; + &apos;-&apos; + &#x3b5;) digits) + &#x3b5;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1395737663396" ID="ID_670012166" MODIFIED="1395737670372" TEXT="opt_exponent = (&apos;E&apos;(&apos;+&apos; + &apos;-&apos;)? digits)?">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1395732972612" ID="ID_483700993" MODIFIED="1395735569922" POSITION="right" TEXT="2. Parsing">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1395732992924" ID="ID_1937702208" MODIFIED="1395735572410" POSITION="right" TEXT="3. Semantic Analysis">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1395733026258" ID="ID_1181544390" MODIFIED="1395735574073" POSITION="right" TEXT="4. Optimization">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1395733115642" ID="ID_1572053387" MODIFIED="1395735576347" POSITION="right" TEXT="5. Code generation">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</map>
