5679924ef88e35a7a283119283e51f90c9aeee53
Andrew Kanner <andrew.kanner@gmail.com>
2014-05-31 16:04:54 +0400

    latex: added mindmaps for Counters/macrocommands and Documents

--- latex.mm
+++ latex.mm
@@ -254,7 +254,7 @@
 <icon BUILTIN="idea"/>
 </node>
 </node>
-<node CREATED="1400909025621" FOLDED="true" ID="ID_883763740" MODIFIED="1400920561568" POSITION="right" TEXT="4. Tables">
+<node CREATED="1400909025621" FOLDED="true" ID="ID_883763740" MODIFIED="1401534963020" POSITION="right" TEXT="4. Tables">
 <node CREATED="1400909739228" ID="ID_955089179" MODIFIED="1400909944539" TEXT="\begin{tabular} {vertival argument1|cc} ... \end{tabular}">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
@@ -337,7 +337,7 @@
 </node>
 <node CREATED="1400919927545" ID="ID_619587428" MODIFIED="1400919970192" TEXT="* - natural width"/>
 </node>
-<node CREATED="1400920620563" FOLDED="true" ID="ID_144523326" MODIFIED="1400921431977" POSITION="right" TEXT="6. Figure wrapping">
+<node CREATED="1400920620563" FOLDED="true" ID="ID_144523326" MODIFIED="1401518280871" POSITION="right" TEXT="6. Figure wrapping">
 <node CREATED="1400920644816" ID="ID_1050297117" MODIFIED="1400921175854" TEXT="\begin{wrapfigure}{l}{0.3333\linewidth}">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="idea"/>
@@ -359,6 +359,256 @@
 <node CREATED="1400921447661" ID="ID_99506713" MODIFIED="1400921463857" TEXT="\listoftables">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1401517383265" FOLDED="true" ID="ID_1733471013" MODIFIED="1401531003724" POSITION="right" TEXT="8. Counters and macro-commands">
+<node CREATED="1401517597520" FOLDED="true" ID="ID_1059139009" MODIFIED="1401519955514" TEXT="numbered lists">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401517612660" ID="ID_310881282" MODIFIED="1401518115458" TEXT="\theoremstyle{plain}"/>
+<node CREATED="1401518115826" ID="ID_961727239" MODIFIED="1401518125129" TEXT="\theoremstyle{definition}"/>
+<node CREATED="1401518125367" ID="ID_791203314" MODIFIED="1401518131789" TEXT="\theoremstyle{remark}"/>
+<node CREATED="1401518178660" ID="ID_1296943773" MODIFIED="1401518261820" TEXT="\newtheorem{NAME}{NAME}[section]">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401518263240" ID="ID_1086762917" MODIFIED="1401518283472" TEXT="section numbering (1.1,1.2,...)">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401518340515" ID="ID_53931944" MODIFIED="1401518728261" TEXT="\newtheorem{NAME}[theorem]{NAME} - continue numbering">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401518392032" ID="ID_338598904" MODIFIED="1401518730741" TEXT="\newtheorem{NAME}{NAME}[theorem] - nested numbering (1.1.1,...)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401518440119" ID="ID_986430663" MODIFIED="1401518468993" TEXT="\newtheorem* - no numeration">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401518568952" ID="ID_175349672" MODIFIED="1401518583273" TEXT="\begin{theorem}...\end{theorem}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401518627177" ID="ID_1636721945" MODIFIED="1401518654500" TEXT="\begin{theorem}[NAME]\label{NAME}...">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1401519956143" FOLDED="true" ID="ID_1504345137" MODIFIED="1401521395165" TEXT="new commands">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401520027211" ID="ID_340547051" MODIFIED="1401520449504" TEXT="\newcommand{\name}[num_args]{definition}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401520175430" ID="ID_1823315559" MODIFIED="1401520240567" TEXT="\ensuremath - enter math-mode where it should be">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401520358943" ID="ID_256875104" MODIFIED="1401520401207" TEXT="&apos;\ &apos; - space I said!">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401520490856" ID="ID_653511792" MODIFIED="1401520504456" TEXT="#1 - argument 1">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401520646712" ID="ID_1002809759" MODIFIED="1401520658676" TEXT="% - to comment newlines"/>
+<node CREATED="1401520813100" ID="ID_905306702" MODIFIED="1401520993607" TEXT="9 arguments, 1 could be unnecessary (first, default value - X) - [9][X]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401521198061" ID="ID_141563390" MODIFIED="1401521235387" TEXT="\renewcommand{\NAME}{redefinition}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1401529211609" FOLDED="true" ID="ID_1896792823" MODIFIED="1401530232515" TEXT="counters">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401529378955" ID="ID_1033990820" MODIFIED="1401529390577" TEXT="\newcounter{name}[section]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401529465127" ID="ID_1227021906" MODIFIED="1401529477010" TEXT="\setcounter{nc}{5}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401529390767" ID="ID_480117462" MODIFIED="1401529535586" TEXT="\arabic{nc} - print counter as arabic number"/>
+<node CREATED="1401529495891" ID="ID_629807828" MODIFIED="1401529528174" TEXT="\Roman{nc} - as roman number"/>
+<node CREATED="1401529498461" ID="ID_662717932" MODIFIED="1401529587641" TEXT="\alph{nc} - as alphabetic letter by number"/>
+<node CREATED="1401529517232" ID="ID_226503840" MODIFIED="1401529615905" TEXT="\Asbuk{nc} - as russian letter by number [babel package]"/>
+<node CREATED="1401529715020" ID="ID_1410179444" MODIFIED="1401529732210" TEXT="\renewcommand{\thesection}{\Asbuk{section}} - renew section numeration symbols">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401530123042" ID="ID_65286907" MODIFIED="1401530125664" TEXT="\addtocounter{nc}{1}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401529733350" ID="ID_184945582" MODIFIED="1401529948173" TEXT="\newcommand{\z}[1]{\addtocounter{nc}{1} &#x417;&#x430;&#x434;&#x430;&#x447;&#x430; \thesection.\arabic{nc}. #1}">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1401530233813" FOLDED="true" ID="ID_469204055" MODIFIED="1401530633003" TEXT="environment">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401530238994" ID="ID_579984129" MODIFIED="1401530383670" TEXT="\newenvironment{TASK_NAME}[num_args]{begin_def}{end_def}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401530371118" ID="ID_1674323526" MODIFIED="1401530447747" TEXT="{\vspace{1cm}} - vertical spacebar 1 cm"/>
+<node CREATED="1401530473317" ID="ID_1902833316" MODIFIED="1401530514365" TEXT="\begin{task}{Main task}..some text..\end{task}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401530547708" ID="ID_432567487" MODIFIED="1401530558141" TEXT="&lt;&lt;#1&gt;&gt; - russian braces"/>
+</node>
+<node CREATED="1401530634211" FOLDED="true" ID="ID_136050627" MODIFIED="1401531000714" TEXT="etoolbox">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401530639142" ID="ID_184485093" MODIFIED="1401530864548" TEXT="\newbool{answers}">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401530649723" ID="ID_580579371" MODIFIED="1401530864548" TEXT="\booltrue{answers}">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401530779501" ID="ID_89327527" MODIFIED="1401530860316" TEXT="\ifbool{answers}{text if true}{text if false}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1401531005643" FOLDED="true" ID="ID_1623574607" MODIFIED="1401538034853" POSITION="right" TEXT="9. Documents">
+<node CREATED="1401532287004" FOLDED="true" ID="ID_588405149" MODIFIED="1401535233540" TEXT="font size">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401531064520" ID="ID_778074581" MODIFIED="1401531216322" TEXT="\documentclass[a4paper,12pt]{article}">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401531197094" ID="ID_1348763778" MODIFIED="1401531207047" TEXT="12pt -&gt; 11pt - ok"/>
+<node CREATED="1401531207495" ID="ID_1714237945" MODIFIED="1401531290431" TEXT="12pt -&gt; 14pt = 10pt, not standart (needs \usepackage{extsizes})">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401533146229" FOLDED="true" ID="ID_1818948961" MODIFIED="1401533173684" TEXT="font size in text">
+<node CREATED="1401533075074" ID="ID_975939288" MODIFIED="1401533075736" TEXT="\tiny"/>
+<node CREATED="1401533075994" ID="ID_1590691099" MODIFIED="1401533084376" TEXT="\scriptsize"/>
+<node CREATED="1401533090375" ID="ID_1967459778" MODIFIED="1401533090977" TEXT="\footnotesize"/>
+<node CREATED="1401533091305" ID="ID_1663525715" MODIFIED="1401533099877" TEXT="\small"/>
+<node CREATED="1401533100135" ID="ID_1894186772" MODIFIED="1401533172104" TEXT="\normalsize - default value"/>
+<node CREATED="1401533106716" ID="ID_1131177720" MODIFIED="1401533114478" TEXT="\large"/>
+<node CREATED="1401533114706" ID="ID_521073491" MODIFIED="1401533120039" TEXT="\Large"/>
+<node CREATED="1401533120267" ID="ID_1443175103" MODIFIED="1401533125359" TEXT="\LARGE"/>
+<node CREATED="1401533130368" ID="ID_83576107" MODIFIED="1401533130920" TEXT="\huge"/>
+<node CREATED="1401533131148" ID="ID_1932362044" MODIFIED="1401533137601" TEXT="\Huge"/>
+</node>
+<node CREATED="1401533175012" ID="ID_1704631130" MODIFIED="1401533292032" TEXT="\Large - works till end of line (or another like \normalsize modificator)">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401533288950" ID="ID_293631046" MODIFIED="1401533336886" TEXT="\begin{Huge}...\end{Huge}">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401533397268" ID="ID_1679923985" MODIFIED="1401533410840" TEXT="{\Large text}">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1401533500595" FOLDED="true" ID="ID_538465422" MODIFIED="1401535229090" TEXT="font modification">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401533517386" ID="ID_522116719" MODIFIED="1401533629977" TEXT="\textbf{text} - bold font">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401533547740" ID="ID_178565331" MODIFIED="1401533635687" TEXT="\textit{text} - italic font">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401533670819" ID="ID_789859083" MODIFIED="1401533756386" TEXT="\emph{text} - emphasis, either italic or bold or..."/>
+<node CREATED="1401533758714" ID="ID_1778757213" MODIFIED="1401533779617" TEXT="\emph{text...\emph{text}...text}"/>
+</node>
+<node CREATED="1401532299525" FOLDED="true" ID="ID_1673015214" MODIFIED="1401535227309" TEXT="document fields">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401531344854" ID="ID_1956513342" MODIFIED="1401531732709" TEXT="\usepackage{geometry} - allow to change document geometry (fields)">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401531389620" ID="ID_1540311469" MODIFIED="1401531755611" TEXT="\geometry{top=25mm} + bottom, left, right and much more"/>
+</node>
+<node CREATED="1401532312787" FOLDED="true" ID="ID_1900278540" MODIFIED="1401535225849" TEXT="document headers and footers">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401531487537" ID="ID_686941841" MODIFIED="1401531662225" TEXT="\usepackage{fancyhdr} - allow change headers and footers">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401531764180" ID="ID_1547283433" MODIFIED="1401531788204" TEXT="there 1 default footer in article document">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401531789942" FOLDED="true" ID="ID_460364552" MODIFIED="1401532168039" TEXT="to activate 6 footers... [not on first page]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401531862477" ID="ID_146450357" MODIFIED="1401531869729" TEXT="lhead{text}"/>
+<node CREATED="1401531802093" ID="ID_1624222697" MODIFIED="1401531874020" TEXT="\lfoot{text}"/>
+<node CREATED="1401531832816" ID="ID_1562428502" MODIFIED="1401531887290" TEXT="\rhead{text}"/>
+<node CREATED="1401531825544" ID="ID_1956269854" MODIFIED="1401531891981" TEXT="\rfoot{text}"/>
+<node CREATED="1401531839706" ID="ID_1230997378" MODIFIED="1401531897241" TEXT="\chead{text}"/>
+<node CREATED="1401531846647" ID="ID_1380485620" MODIFIED="1401531914131" TEXT="\cfoot{text} - here is page number as default"/>
+</node>
+<node CREATED="1401532012797" ID="ID_1436558837" MODIFIED="1401532103053" TEXT="renewcommand{\headrulewidth}{0mm} - disable header line as default">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1401532337069" FOLDED="true" ID="ID_735936068" MODIFIED="1401535224449" TEXT="line spacing">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401532369972" ID="ID_824153444" MODIFIED="1401532380914" TEXT="\usepackage{setspace}"/>
+<node CREATED="1401532381142" ID="ID_1396906533" MODIFIED="1401532478408" TEXT="\onehalfspacing - 1.5"/>
+<node CREATED="1401532390202" ID="ID_465646875" MODIFIED="1401532482020" TEXT="\doublespacing - 2"/>
+<node CREATED="1401532398563" ID="ID_1860099352" MODIFIED="1401532486190" TEXT="\singlespacing - 1.5, as the default"/>
+</node>
+<node CREATED="1401532498218" FOLDED="true" ID="ID_1291710194" MODIFIED="1401535223019" TEXT="last line counter">
+<node CREATED="1401532513650" ID="ID_1852684660" MODIFIED="1401532522295" TEXT="\usepackage{lastpage}"/>
+</node>
+<node CREATED="1401532540793" FOLDED="true" ID="ID_1935548309" MODIFIED="1401535221919" TEXT="text modifications">
+<node CREATED="1401532549654" ID="ID_1035145504" MODIFIED="1401532553207" TEXT="\usepackage{soulutf8}"/>
+<node CREATED="1401532633521" ID="ID_1801407115" MODIFIED="1401532665846" TEXT="\renewcommand{\familydefault}{\sfdefault} - serif text modification">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1401532757747" ID="ID_746390402" MODIFIED="1401532765511" TEXT="arial - without serif"/>
+<node CREATED="1401532765809" ID="ID_887163634" MODIFIED="1401532773922" TEXT="times new roman - with serif"/>
+</node>
+<node CREATED="1401532555525" FOLDED="true" ID="ID_1360733032" MODIFIED="1401534772208" TEXT="hyperlinks">
+<node CREATED="1401532561747" ID="ID_1082093476" MODIFIED="1401532569809" TEXT="\usepackage{hyperref}"/>
+<node CREATED="1401534510982" ID="ID_777439083" MODIFIED="1401534517914" TEXT="\url{link}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401534518634" ID="ID_66162619" MODIFIED="1401534719364" TEXT="\href{link}{text}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1401532707116" FOLDED="true" ID="ID_734885419" MODIFIED="1401535532300" TEXT="multicolumn text">
+<node CREATED="1401535200575" ID="ID_1267479202" MODIFIED="1401535256941" TEXT="\documentclass[a4paper,12pt,twocolumt]{article} - all document with two-column text"/>
+<node CREATED="1401535299262" ID="ID_1209128830" MODIFIED="1401535327377" TEXT="\newpage in twocolumn document will start new column">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401532717016" ID="ID_445221866" MODIFIED="1401535458583" TEXT="\usepackage{multicol}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401535045374" ID="ID_1793739413" MODIFIED="1401535462643" TEXT="\begin{multicols}{2}...\end{multicols}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401535070206" ID="ID_328508059" MODIFIED="1401535078539" TEXT="\item"/>
+</node>
+<node CREATED="1401533919405" FOLDED="true" ID="ID_70876912" MODIFIED="1401534477473" TEXT="title page">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401533930105" ID="ID_956840372" MODIFIED="1401533946387" TEXT="\newpage"/>
+<node CREATED="1401533946635" ID="ID_1416698295" MODIFIED="1401533983809" TEXT="\thispagestyle{empty} - no footer and header"/>
+<node CREATED="1401533987927" ID="ID_639242499" MODIFIED="1401534072654" TEXT="\begin{center}...\end{center} - center alignment"/>
+<node CREATED="1401534163566" ID="ID_1813701252" MODIFIED="1401534214920" TEXT="flushright - right alignment"/>
+<node CREATED="1401534112394" ID="ID_1138008784" MODIFIED="1401534179379" TEXT="\\ - line endings"/>
+<node CREATED="1401534113294" ID="ID_1575492611" MODIFIED="1401534121516" TEXT="\vspace{0.5ex}"/>
+<node CREATED="1401534248740" ID="ID_713189582" MODIFIED="1401534265702" TEXT="\noindent - no first line special alignment">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1401534324303" ID="ID_388692081" MODIFIED="1401534356740" TEXT="\so - text "/>
+<node CREATED="1401534348387" ID="ID_1470983125" MODIFIED="1401534352509" TEXT="\, - space"/>
+<node CREATED="1401534379880" ID="ID_1171639865" MODIFIED="1401534441153" TEXT="\vfill - fill till end of the page (for some text in the end of page)"/>
+</node>
+<node CREATED="1401534797169" FOLDED="true" ID="ID_122658698" MODIFIED="1401535154274" TEXT="itemizes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1401534816841" ID="ID_1722544863" MODIFIED="1401534901325" TEXT="\begin{itemize}...\end{itemize} - bulleted list">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401534827501" ID="ID_454435206" MODIFIED="1401534864994" TEXT="enumerate, description, list"/>
+<node CREATED="1401534883812" ID="ID_1818001958" MODIFIED="1401535120162" TEXT="\item[*] - insert * as the bullet point"/>
+<node CREATED="1401534941367" ID="ID_1380301870" MODIFIED="1401535132272" TEXT="item can be new \begin{itemize} and so on">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1401535928009" FOLDED="true" ID="ID_917605394" MODIFIED="1401538032802" TEXT="landscape view">
+<node CREATED="1401535936639" ID="ID_1552230106" MODIFIED="1401538031687" TEXT="\usepackage{pdflscape} - or lscape as well"/>
+<node CREATED="1401535957620" ID="ID_68631170" MODIFIED="1401535973464" TEXT="\begin{landscape}...\end{landscape}"/>
+</node>
+</node>
+<node CREATED="1401536357350" ID="ID_1999667694" MODIFIED="1401536392919" POSITION="right" TEXT="addcontentsline{file}{sec_unit}{entry}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1401521356808" FOLDED="true" ID="ID_1587163883" MODIFIED="1401529143142" POSITION="right" TEXT="z. Rissuan typographics">
+<node CREATED="1401521301622" ID="ID_255511817" MODIFIED="1401521305760" TEXT="\renewcommand{\ge}{\geqslant}">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </node>
